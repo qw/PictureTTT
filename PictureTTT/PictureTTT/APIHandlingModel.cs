612856b7644d6e88acbbcf851e5cd0484fe2ef95
Impure <redimpure@gmail.com>
2017-07-14 21:43:01 +1200

    Refactored to MVVM. Now to implement communication to second API.

---
+++ PictureTTT/PictureTTT/APIHandlingModel.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System.Reflection;
+
+namespace PictureTTT
+{
+    class APIHandlingModel
+    {
+        private List<APIHandlingListener> listeners = new List<APIHandlingListener>() { };
+
+        private static APIHandlingModel instance;
+        public static APIHandlingModel Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new APIHandlingModel();
+                }
+
+                return instance;
+            }
+        }
+
+        //Subscription key and region
+        const string subscriptionKey = "0f20ae1d128643d492ad1af03d13d616";
+        const string uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/ocr";
+
+        public string contentString { get; set; }
+
+        private APIHandlingModel() { }
+
+        public async Task uploadFromGallery(String imagePath)
+        {
+            //Converts local image to bytearray
+            byte[] byteData = null;
+            var assembly = this.GetType().GetTypeInfo().Assembly;
+            string[] resourceNames = GetType().GetTypeInfo().Assembly.GetManifestResourceNames();
+            using (Stream s = assembly.GetManifestResourceStream(imagePath))
+            {
+                long length = s.Length;
+                byteData = new byte[(int)length];
+                s.Read(byteData, 0, (int)length);
+            }
+
+             await MakeOCRRequest(byteData);
+        }
+
+        public async Task uploadFromCamera(MediaFile file)
+        {
+            if (file == null)
+                return;
+
+            //Converts MediaFile into byte array
+            var stream = file.GetStream();
+            BinaryReader binaryReader = new BinaryReader(stream);
+            byte[] byteData = binaryReader.ReadBytes((int)stream.Length);
+
+            await MakeOCRRequest(byteData);
+            file.Dispose();
+        }
+         
+        public async Task MakeOCRRequest(byte[] byteData)
+        {
+            HttpClient client = new HttpClient();
+
+            // Request headers.
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // Request parameters.
+            string requestParameters = "language=unk&detectOrientation=true";
+
+            // Assemble the URI for the REST API Call.
+            string uri = uriBase + "?" + requestParameters;
+
+            HttpResponseMessage response;
+
+            using (ByteArrayContent content = new ByteArrayContent(byteData))
+            {
+                // Header Content-Type "application/octet-stream" is local image file
+                // "application/json" is image url
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                // Execute the REST API call.
+                response = await client.PostAsync(uri, content);
+
+                // Get the JSON response.
+                contentString = await response.Content.ReadAsStringAsync();
+            }
+
+            //extract english from JSON response
+            //do API call to Translate service
+        }
+
+        public void addListener(APIHandlingListener listener)
+        {
+            listeners.Add(listener);
+        }
+
+        private void updateListeners()
+        {
+            foreach (APIHandlingListener l in listeners)
+            {
+                l.update();
+            }
+        }
+
+    }
+}
--- PictureTTT/PictureTTT/AzureTable.xaml.cs
+++ PictureTTT/PictureTTT/AzureTable.xaml.cs
@@ -15,19 +15,17 @@ using System.Reflection;
 namespace PictureTTT
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class AzureTable : ContentPage
+	public partial class AzureTable : ContentPage, APIHandlingListener
 	{
-        //Subscription key and region
-        const string subscriptionKey = "0f20ae1d128643d492ad1af03d13d616";
-        const string uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/ocr";
-
-        public string contentString { get; set; }
+        private APIHandlingModel model = APIHandlingModel.Instance;
+        private const string localImagePath = "PictureTTT.upload.png";
         private MediaFile file = null;
 
         public AzureTable()
 		{
             NavigationPage.SetHasNavigationBar(this, false);
 			InitializeComponent();
+            model.addListener(this);
             image.Source = "test.jpg";
         }
 
@@ -56,7 +54,7 @@ namespace PictureTTT
 
             image.Source = ImageSource.FromStream(() =>
             {
-               return file.GetStream();
+                return file.GetStream();
             });
 
             //file.Dispose();
@@ -66,116 +64,27 @@ namespace PictureTTT
         //Display response in Translation page
         private async void uploadImage(object sender, EventArgs e)
         {
-            //Only if photo is taken
+            uploadingBusy.IsRunning = true;
+
             if (file == null)
             {
-                //Temporary debug action 
-                uploadingBusy.IsRunning = true;
-                HttpClient client = new HttpClient();
-
-                // Request headers.
-                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
-
-                // Request parameters.
-                string requestParameters = "language=unk&detectOrientation=true";
-
-                // Assemble the URI for the REST API Call.
-                string uri = uriBase + "?" + requestParameters;
-
-                HttpResponseMessage response;
-
-                //converts local image to bytearray
-                byte[] byteData = null;
-                try
-                {
-                    var assembly = this.GetType().GetTypeInfo().Assembly;
-                    string[] resourceNames = GetType().GetTypeInfo().Assembly.GetManifestResourceNames();
-                    using (Stream s = assembly.GetManifestResourceStream("PictureTTT.upload.png"))
-                    {
-                        long length = s.Length;
-                        byteData = new byte[(int)length];
-                        s.Read(byteData, 0, (int)length);
-                    }
-                } catch (Exception error)
-                {
-                    await DisplayAlert("!", "!", "OK");
-                    System.Diagnostics.Debug.WriteLine("#######: "+ error.Message);
-                }
-
-                using (ByteArrayContent content = new ByteArrayContent(byteData))
-                    {
-                        // Header Content-Type "application/octet-stream" is local image file
-                        // "application/json" is image url
-                        content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
-
-                        // Execute the REST API call.
-                        response = await client.PostAsync(uri, content);
-
-                        // Get the JSON response.
-                        contentString = await response.Content.ReadAsStringAsync();
-                        jsonResponse.Text = contentString;
-
-                    }
-
-                uploadingBusy.IsRunning = false;
-
-                //   await DisplayAlert("No Image!", "Please take a photo first", "OK");
+                await model.uploadFromGallery(localImagePath);
+                //await DisplayAlert("No Image!", "Please take a photo first", "OK");
                 //return;
             }
             else
             {
-                await MakeOCRRequest(file);
-                file.Dispose();
-            }
-        }
-
-        private static byte[] GetImageAsByteArray(MediaFile file)
-        {
-            var stream = file.GetStream();
-            BinaryReader binaryReader = new BinaryReader(stream);
-            return binaryReader.ReadBytes((int)stream.Length);
-        }
-
-        private async Task MakeOCRRequest(MediaFile file)
-        {
-            if (file == null)
-                return;
-
-            uploadingBusy.IsRunning = true;
-            HttpClient client = new HttpClient();
-
-            // Request headers.
-            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
-
-            // Request parameters.
-            string requestParameters = "language=unk&detectOrientation=true";
-
-            // Assemble the URI for the REST API Call.
-            string uri = uriBase + "?" + requestParameters;
-
-            HttpResponseMessage response;
-
-            // Request body. Posts a locally stored JPEG image.
-            byte[] byteData = GetImageAsByteArray(file);
-
-            using (ByteArrayContent content = new ByteArrayContent(byteData))
-            {
-                // Header Content-Type "application/octet-stream" is local image file
-                // "application/json" is image url
-                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
-
-                // Execute the REST API call.
-                response = await client.PostAsync(uri, content);
-
-                // Get the JSON response.
-                contentString = await response.Content.ReadAsStringAsync();
-                jsonResponse.Text = contentString;
-
+                await model.uploadFromCamera(file);
             }
 
+            jsonResponse.Text = model.contentString;
             uploadingBusy.IsRunning = false;
         }
 
+        public async void update()
+        {
+            await DisplayAlert("Upload Complete!", "Upload Complete!", "OK");
+        }
     }
 
 }
